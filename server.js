663058650baf98a814728c9f0e38effe37fa2b69
Availablecoder <ahmed.cross20@gmail.com>
2022-08-31 13:40:56 +0200

    Changing After Deploying

--- server.js
+++ server.js
@@ -7,9 +7,7 @@ const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
   res.end(
-    `This is Testing Nodejs app deployment from github ripos\n lets see if it can determint the procces.env.MY_WAR=${
-      process.env.MY_WAR || "unable to see MY_WAR"
-    }`
+    `We Changed Some things ${process.env.MY_WAR || "unable to see MY_WAR"}`
   );
 });
 
